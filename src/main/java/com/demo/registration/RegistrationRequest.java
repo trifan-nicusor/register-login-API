305fef7b5286a4ce2c989b5f7e2c61a9e010fb1b
trifan-nicusor <nicusormadalintrifan@yahoo.com>
2023-02-28 14:57:19 +0200

    Code refactoring

--- src/main/java/com/demo/registration/RegistrationRequest.java
+++ src/main/java/com/demo/registration/RegistrationRequest.java
@@ -1,8 +1,9 @@
 package com.demo.registration;
 
-public record RegistrationRequest(String firstName,
-                                  String lastName,
-                                  String email,
-                                  String password) {
+public record RegistrationRequest(
+        String firstName,
+        String lastName,
+        String email,
+        String password) {
 
 }
